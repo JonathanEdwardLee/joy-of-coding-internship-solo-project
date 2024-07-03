adcfd9faa6a9a916167a8e24d81c1fe2da51ce68
Jonathan Edward Lee <94152189+JonathanEdwardLee@users.noreply.github.com>
2024-07-02 23:27:48 -0500

    repaired task entering bug

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,11 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ ...task, username }),
+      body: JSON.stringify({
+        description: task.description,
+        dueDate: task.dueDate,
+        username,
+      }),
     })
       .then((response) => {
         if (!response.ok) {
